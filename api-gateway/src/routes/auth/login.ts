2a24b400fdaea53ab0f6b6fd43cc1a557eafc516
tavom17 <tavo.mejia17@gmail.com>
2026-04-09 16:14:19 -0400

    jwt access and refresh token for login

--- api-gateway/src/routes/auth/login.ts
+++ api-gateway/src/routes/auth/login.ts
@@ -1,18 +1,20 @@
 import { FastifyInstance } from "fastify"
 import pool from "../../db"
 import bcrypt from "bcrypt"
-
+import * as jwt from "jsonwebtoken"
+//maintain user structure, especially for querying db
 interface user {
+  user_id: string
   user_email: string
   user_password_hash: string
 }
 
 export async function loginRoute(fastify: FastifyInstance) {
   fastify.post("/login", async (request, reply) => {
-    const { username, password } = request.body as { username: string, password: string }
+    const { user_email, password } = request.body as { user_email: string, password: string }
 
     try {
-      const dbQuery = await pool.query<user>('select user_email, user_password_hash from tb_users where user_email = $1', [username])
+      const dbQuery = await pool.query<user>('select user_id,user_email, user_password_hash from tb_users where user_email = $1', [user_email])
 
       if (!dbQuery.rowCount || dbQuery.rowCount === 0) {
         return reply.status(401).send({ error: "Invalid credentials" })
@@ -26,7 +28,24 @@ export async function loginRoute(fastify: FastifyInstance) {
         return reply.status(401).send({ error: "Invalid credentials" })
       }
 
-      return reply.send({ accessToken: "mock-token-for-now" })
+      //extract info for jwt token now that password hash is valid
+      //already have user_email from request body
+      const user_id = dbQuery.rows[0]?.user_id;      
+    
+      // access token — short lived, used for API requests
+const accessToken = jwt.sign(
+    { user_id, user_email },      // payload
+    process.env.JWT_ACCESS_SECRET!,  // secret
+    { expiresIn: '15m' }    // options
+)
+
+// refresh token — long lived, used to get new access tokens
+const refreshToken = jwt.sign(
+    { user_id },
+    process.env.JWT_REFRESH_SECRET!,
+    { expiresIn: '7d' }
+)
+      return reply.send({ accessToken, refreshToken })
     } catch (err) {
       fastify.log.error(err)
       return reply.status(500).send({ error: "Internal server error" })
